f5e5cfc2e12c317184c079e6bf0fc3db6b4b9875
Katuhinx <145397681+Katuhinx@users.noreply.github.com>
2024-05-29 04:29:03 +0000

    part 3 v 3.5

--- src/test/java/com/pismennaya/shop/DAO/ClientDAOTest.java
+++ src/test/java/com/pismennaya/shop/DAO/ClientDAOTest.java
@@ -14,8 +14,61 @@ public class ClientDAOTest {
     private ClientDAO clientDAO;
 
     @Test
-    public void testGetByFilters() {
-        List<Client> clients = clientDAO.getByFilters("testlogin");
+    public void testGetById() {
+        Client client = clientDAO.getById(1);
+        Assert.assertNotNull(client);
+        Assert.assertEquals(1, client.getId().longValue());
+    }
 
+    @Test
+    public void testGetAll() {
+        List<Client> clients = clientDAO.getAll();
+        Assert.assertNotNull(clients);
+        Assert.assertTrue(clients.size() > 0);
+    }
+
+    @Test
+    public void testSave() {
+        Client newClient = new Client();
+        newClient.setName("Test1");
+        newClient.setPhone("89123456701");
+        clientDAO.save(newClient);
+
+        Client savedClient = clientDAO.getById(newClient.getId());
+        Assert.assertNotNull(savedClient);
+        Assert.assertEquals(newClient.getName(), savedClient.getName());
+        Assert.assertEquals(newClient.getPhone(), savedClient.getPhone());
+    }
+
+    @Test
+    public void testUpdate() {
+        Client client = clientDAO.getById(1);
+        client.setName("Test2");
+        clientDAO.update(client);
+        Client updatedClient = clientDAO.getById(1);
+        Assert.assertEquals("Test2", updatedClient.getName());
+    }
+
+    @Test
+    public void testDelete() {
+        Client client = clientDAO.getById(1);
+        clientDAO.delete(client);
+        Client deletedClient = clientDAO.getById(1);
+        Assert.assertNull(deletedClient);
+    }
+
+    @Test
+    public void testGetByFilters() {
+        List<Client> clients = clientDAO.getByFilters("Test1");
+        Assert.assertNotNull(clients);
+        Assert.assertTrue(clients.size() > 0);
+
+    }
+
+    @Test
+    public void testGetByPhone() {
+        Client client = clientDAO.getByPhone("89123456701");
+        Assert.assertNotNull(client);
+        Assert.assertEquals("89123456701", client.getPhone());
     }
 }
