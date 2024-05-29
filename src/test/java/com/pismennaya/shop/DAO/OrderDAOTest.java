20e60d295984f511811c9c7b4591f6c1f97fad96
Katuhinx <145397681+Katuhinx@users.noreply.github.com>
2024-05-29 05:08:01 +0000

    part 3 v 3.8

---
+++ src/test/java/com/pismennaya/shop/DAO/OrderDAOTest.java
@@ -0,0 +1,32 @@
+package com.pismennaya.shop.DAO;
+
+import com.pismennaya.shop.interfaces.OrderDAO;
+import com.pismennaya.shop.interfaces.impl.OrderDAOImpl;
+import com.pismennaya.shop.models.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class OrderDAOTest {
+    @Autowired
+    private OrderDAO orderDAO = new OrderDAOImpl();
+
+    @Test
+    public void testGetByFilters() {
+        List<Order> orders = orderDAO.getByFilters(1);
+        Assert.assertNotNull(orders);
+        Assert.assertTrue(orders.size() > 0);
+    }
+
+    @Test
+    public void testGetByClient() {
+        List<Order> orders = orderDAO.getByClient(1);
+        Assert.assertNotNull(orders);
+        Assert.assertTrue(orders.size() > 0);
+    }
+}
